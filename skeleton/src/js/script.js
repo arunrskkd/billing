49010cbe476de9b1c04dea55691a90cf9d985f76
arun <arunrs123@gmail.com>
2018-04-12 14:00:47 +0530

    asd

---
+++ skeleton/src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+		//button  click hides the secthide section
+          $(".butn").click(function(){
+         	$(this).hide();
+
+          });
+          
+
+});
+
